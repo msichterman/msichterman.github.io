aab58569bfab9441cdb2e6b2213a14c3acd24db0
Matthew Sichterman <msichterman1@gmail.com>
2019-06-24 22:46:32 -0500

    Loading screen added

--- main.js
+++ main.js
@@ -1,4 +1,12 @@
+window.addEventListener("load", () => {
+    const loading = document.querySelector(".loading");
+    loading.className += " loaded";
+    const scroll = document.querySelector(".scroll-off");
+    scroll.classList.remove("scroll-off");
+});
+
 const hamburger = document.getElementById("hamburger");
 hamburger.addEventListener("click", () => {
-    hamburger.classList.toggle("is-active");
+   hamburger.classList.toggle("is-active");
 });
+
