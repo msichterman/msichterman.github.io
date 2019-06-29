59c380d4ab9aa69da0ce9b810e57893cdfd620b4
Matthew Sichterman <msichterman1@gmail.com>
2019-06-28 21:59:15 -0500

    Top link and menu navigation implemented

--- main.js
+++ main.js
@@ -6,7 +6,17 @@ window.addEventListener("load", () => {
 });
 
 const hamburger = document.getElementById("hamburger");
+const menu = document.querySelector(".expandable-menu");
 hamburger.addEventListener("click", () => {
    hamburger.classList.toggle("is-active");
+
+   menu.classList.toggle("show");
+});
+
+const menuItems = document.getElementsByClassName("menu-item");
+Array.from(menuItems).forEach( (item) => {
+    item.addEventListener("click", () => {
+    menu.classList.toggle("show");
+    });
 });
 
