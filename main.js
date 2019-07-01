abbbbfbb95949fd44af80cece91eef8e95ef6a09
Matthew Sichterman <msichterman1@gmail.com>
2019-06-30 22:24:40 -0500

    Loading screen scroll bug fixed. Hamburger menu bug fixed.

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 window.addEventListener("load", () => {
-    const loading = document.querySelector(".loading");
-    loading.className += " loaded";
+    const loaderContainer = document.querySelector(".loader-container");
+    const spinner = document.querySelector(".spinner");
+    loaderContainer.className += " loaded";
+    spinner.className += " loaded";
     const scroll = document.querySelector(".scroll-off");
     scroll.classList.remove("scroll-off");
 });
@@ -9,7 +11,6 @@ const hamburger = document.getElementById("hamburger");
 const menu = document.querySelector(".expandable-menu");
 hamburger.addEventListener("click", () => {
    hamburger.classList.toggle("is-active");
-
    menu.classList.toggle("show");
 });
 
@@ -17,6 +18,7 @@ const menuItems = document.getElementsByClassName("menu-item");
 Array.from(menuItems).forEach( (item) => {
     item.addEventListener("click", () => {
     menu.classList.toggle("show");
+    hamburger.classList.toggle("is-active");
     });
 });
 
